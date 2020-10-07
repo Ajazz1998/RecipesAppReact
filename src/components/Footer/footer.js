8c49dafc23d1bc596c2d27e2239e743eb70fce38
Ajay Bava <ajaybava1998@gmail.com>
2020-10-07 14:12:25 +0100

    coomit

---
+++ src/components/Footer/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './footer.css';
+
+const footer = () => {
+  return(
+    <footer className="main-footer">
+
+      <section className="main-footer">
+
+        
+
+      </section>
+
+    </footer>
+  );
+};
+
+export default footer;
