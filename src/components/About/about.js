ea021c3a64e3563ec86cfca65a7bfdae5db4a79d
Ajay Bava <ajaybava1998@gmail.com>
2020-10-09 19:03:32 +0100

    update

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './about.css'
 
 const log = () => {
-    return (
-        <div>
-            <h1>About</h1>
-        </div>
-    )
+  return (
+    <div className="about">
+      <div className="container">
+          <div className="col-lg-7">
+            <img
+              className="img"
+              src="https://www.w3schools.com/howto/img_avatar.png"
+              alt="img"
+            />
+          </div>
+          <div className="col-lg-5">
+            <h1 className="font-weight-light">About</h1>
+            <p>
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s, when an unknown printer took a galley of
+              type and scrambled it to make a type specimen book.
+            </p>
+          </div>
+      </div>
+    </div>
+  );
 }
 
 export default log;
